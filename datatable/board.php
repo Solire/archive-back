59bc289f54a5fc956890e5fc1c0fa0800b7b1003
Monnot Stéphane <smonnot@debian-dev.(none)>
2013-03-01 14:56:34 +0100

    Correction des urls dans le datatable du dashboard

--- datatable/board.php
+++ datatable/board.php
@@ -70,14 +70,14 @@ class Board extends \Slrfw\Datatable\Datatable {
         $actionHtml = '<div style="width:110px">';
 
         if (($this->_utilisateur != null && $this->_utilisateur->get("niveau") == "solire") || ($this->_gabarits != null && $this->_gabarits[$data["id_gabarit"]]["editable"])) {
-            $actionHtml .= '<div class="btn-a btn-mini gradient-blue fl" ><a title="Modifier" href="page/display.html?id_gab_page=' . $data["id"] . '"><img alt="Modifier" src="app/back/img/white/pen_alt_stroke_12x12.png" /></a></div>';
+            $actionHtml .= '<div class="btn-a btn-mini gradient-blue fl" ><a title="Modifier" href="back/page/display.html?id_gab_page=' . $data["id"] . '"><img alt="Modifier" src="app/back/img/white/pen_alt_stroke_12x12.png" /></a></div>';
         }
         if (($this->_utilisateur->get("niveau") == "solire" || $this->_gabarits[$data["id_gabarit"]]["make_hidden"] || $data["visible"] == 0) && $data["rewriting"] != "") {
             $actionHtml .= '<div class="btn-a btn-mini gradient-blue fl" ><a title="Rendre visible \'' . $data["titre"] . '\'" style="padding: 3px 7px 3px;"><input type="checkbox" value="' . $data["id"] . '-' . $data["id_version"] . '" class="visible-lang visible-lang-' . $data["id"] . '-' . $data["id_version"] . '" ' . ($data["visible"] > 0 ? ' checked="checked"' : '') . '/></a></div>';
         }
 
         if($data["suppr"] == 1) {
-            $actionHtml = '<div class="btn-a btn-mini gradient-blue fl" ><a title="Modifier" href="page/undelete.html?id_gab_page=' . $data["id"] . '"><img alt="Récupérer" src="app/back/img/white/pen_alt_stroke_12x12.png" /></a></div>';
+            $actionHtml = '<div class="btn-a btn-mini gradient-blue fl" ><a title="Modifier" href="back/page/undelete.html?id_gab_page=' . $data["id"] . '"><img alt="Récupérer" src="app/back/img/white/pen_alt_stroke_12x12.png" /></a></div>';
 
         }
 
@@ -101,9 +101,9 @@ class Board extends \Slrfw\Datatable\Datatable {
 
 
         if ($data["rewriting"] == "") {
-            $actionHtml .= '<div class="btn-a btn-mini gradient-red"><a style="color:white;line-height: 12px;" href="page/display.html?id_gab_page=' . $data["id"] . '">Non traduit</a></div>';
+            $actionHtml .= '<div class="btn-a btn-mini gradient-red"><a style="color:white;line-height: 12px;" href="back/page/display.html?id_gab_page=' . $data["id"] . '">Non traduit</a></div>';
         } else {
-            $actionHtml .= '<div class="btn-a btn-mini gradient-green"><a style="color:white;line-height: 12px;" href="page/display.html?id_gab_page=' . $data["id"] . '">Traduit</a></div>';
+            $actionHtml .= '<div class="btn-a btn-mini gradient-green"><a style="color:white;line-height: 12px;" href="back/page/display.html?id_gab_page=' . $data["id"] . '">Traduit</a></div>';
         }
         $actionHtml .= '</div>';
         return $actionHtml;
