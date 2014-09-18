b1cf39c6f1cbeb1512d4b269811ef186ed842194
Shinbuntu <smonnot@solire.fr>
2014-09-18 11:27:41 +0200

    Correction sur la configuration de pageModule avec id version > 1

--- controller/page.php
+++ controller/page.php
@@ -597,7 +597,7 @@ class Page extends Main
              * Si le gabarit courant appartien à un des groupes personnalisés
              */
             if ($currentConfigPageModule['gabarits'] == '*'
-                || in_array($this->_pages[1]->getGabarit()->getId(), $currentConfigPageModule['gabarits'])
+                || in_array($this->_pages[BACK_ID_VERSION]->getGabarit()->getId(), $currentConfigPageModule['gabarits'])
             ) {
                 $indexPageList = $index;
                 $found = true;
